bd25e51d5cdf23d4d40746ce70a2a92bb40d1e4e
Mayur Patel <pmayur0680@gmail.com>
2022-07-15 19:42:49 -0500

    handling endGame and resetGame

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 let currentPlayer = 1;
+let numMovesDone = 0;
 // create empty 3x3 board
 let boardState = Array.from(Array(3), () => new Array(3));
 // select elements
 const gameHeading = document.getElementById('game-heading');
 const gameSquares = document.querySelectorAll('.game-square');
 const restartButton = document.getElementById('restart-button');
-
+restartButton.addEventListener('click', restartGame);
 // get row, col clicked for each button
 gameSquares.forEach( (gameSquare, i) => {
     gameSquare.addEventListener('click', () => {
@@ -20,8 +21,44 @@ function makeMove(gameSquare, row, col) {
     // based on player 1/2, set X/O
     gameSquare.textContent = currentPlayer === 1 ? 'X': 'O';
     boardState[row][col] = currentPlayer;
-    // toggle player
-    currentPlayer = currentPlayer === 1 ? 2: 1;
-    gameHeading.textContent = `Player ${currentPlayer}'s Turn`;
-    console.log(boardState);
+    numMovesDone++;
+    // check for condition if user win, else if game tied else move turn to next player
+    if(didPlayerWin()) {
+        gameHeading.textContent = `Player ${currentPlayer} WON`;     
+        endGame();
+    } else if(numMovesDone >= 9) {
+        // game tied, no more choice available        
+        gameHeading.textContent = `Tie Game!`;  
+        endGame();        
+    } else {
+        // toggle player
+        currentPlayer = currentPlayer === 1 ? 2: 1;
+        gameHeading.textContent = `Player ${currentPlayer}'s Turn`;       
+    } 
+}    
+
+function didPlayerWin() {
+    // temp return false, will implement soon
+    return false;
+}
+
+function endGame() {
+    // enable reset button and disable the board
+    restartButton.style.display = 'block';
+    gameSquares.forEach(gameSquare => {
+    gameSquare.disabled = true;
+    })      
+}
+
+function restartGame() {
+ // reset board and states   
+ boardState = Array.from(Array(3), () => new Array(3));
+ currentPlayer = 1;
+ numMovesDone = 0;
+ gameHeading.textContent = `Player ${currentPlayer}'s Turn`; 
+ gameSquares.forEach(gameSquare => {
+    gameSquare.textContent = '';
+    gameSquare.disabled = false;
+ })        
+ restartButton.style.display = 'none';
 }
